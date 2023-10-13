8294f376806360ff09f1c5b84faf181a6cf41901
Moses-Yu <mosesyumss@gmail.com>
2023-10-13 22:33:06 +0900

    vscode setup, 메뉴 컨트롤러 작업중

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject[] menuObjects;
+    private int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("horizontal") > 0){
+
+        }
+
+        
+    }
+
+    
+}
