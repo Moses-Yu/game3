e30aac376cf852ec26ef5bdc68035dffc0ec5536
Moses-Yu <mosesyumss@gmail.com>
2023-10-18 06:31:20 +0900

    fixed update로 노트 이동 구현 GameController에서 노트 속도 조절가능

---
+++ Assets/Note.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    private GameController gameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.Find("game").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // transform.localPosition = new Vector3(0, transform.localPosition.y - (GameController.noteSpeed * 0.01f), 0);
+        transform.Translate(new Vector3(0, - (gameController.noteSpeed * 0.001f), 0));
+    }
+}
