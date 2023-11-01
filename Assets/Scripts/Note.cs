c223d149e40f3d3cd174bc52912c6511aef01544
Moses-Yu <mosesyumss@gmail.com>
2023-11-01 19:11:37 +0900

    Time step을 사용한 노트 이동 계산

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -12,21 +12,20 @@ public class Note : MonoBehaviour
         gameController = GameObject.Find("game").GetComponent<GameController>();
     }
 
-    private bool passhitline = false;
+    // private bool passhitline = false;
     // Update is called once per frame
     void FixedUpdate()
     {
         // transform.localPosition = new Vector3(0, transform.localPosition.y - (GameController.noteSpeed * 0.01f), 0);
 
-
-
-        if (!passhitline) {
-            if(transform.localPosition.y < -10f){
-                // print("passhitline " + (transform.localPosition.y + 10));
-                passhitline = true;
-            }
-        }
-        transform.Translate(new Vector3(0, -(gameController.noteSpeed * 0.001f), 0));
+        // if (!passhitline) {
+        //     if(transform.localPosition.y < -10f){
+        //         // print("passhitline " + (transform.localPosition.y + 10));
+        //         passhitline = true;
+        //     }
+        // }
+        // print(Time.fixedDeltaTime);
+        transform.Translate(new Vector3(0, -(gameController.noteSpeed * 0.001f * Time.fixedDeltaTime), 0));
 
         if(transform.localPosition.y < -15f){
             Destroy(gameObject);
